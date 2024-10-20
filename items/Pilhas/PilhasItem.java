5dea003ab151e12852bdda94b0e3ef02230ced9a
samuskox <samu5020ol@gmail.com>
2024-10-19 22:08:06 -0300

    Pilha feita

--- items/Pilhas/PilhasItem.java
+++ items/Pilhas/PilhasItem.java
@@ -2,12 +2,17 @@ package items.Pilhas;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
 
 import engine.abstractclasses.GraphicsItem;
 import engine.util.GraphicsPanel;
 import engine.variables.Vector2;
 import items.ZoomScale;
 import java.util.Stack;
+
+import javax.imageio.ImageIO;
 
 public class PilhasItem extends GraphicsItem{
 
@@ -17,7 +22,18 @@ public class PilhasItem extends GraphicsItem{
     private Vector2 size = new Vector2(100, 50);
     private static final int SPACEMENT = 10;
 
+    private static Image arrowImg;
+    private static final int ARROW_IMG_WIDTH = 426 / 4;
+    private static final int ARROW_IMG_HEIGHT = 213 / 2;
+
     PilhasItem(){
+
+        try {
+            arrowImg = ImageIO.read(new File("assets\\arrow.png"));
+            arrowImg = arrowImg.getScaledInstance(ARROW_IMG_WIDTH, ARROW_IMG_HEIGHT, Image.SCALE_DEFAULT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         position = new Vector2(GraphicsPanel.getPanelWidth() / 2, GraphicsPanel.getPanelHeight());
     }
 
@@ -27,19 +43,23 @@ public class PilhasItem extends GraphicsItem{
 
     @Override
     public void draw(Graphics2D g2) {
-        // for (int i = 0; i < pilha.size(); i++) {
-        //     //position.y = (size.y * i) + SPACEMENT * (i + 1);
-        //     position.y = (size.y * (i+1));
-        //     g2.drawString("Index: " + Integer.toString(i), (int) position.x, (int) (position.y - 2));
-        //     g2.drawString("Valor: " + pilha.get(i), (int) position.x, (int) (position.y + size.y + SPACEMENT));
-        //     g2.setColor(Color.DARK_GRAY);
-        //     g2.fillRoundRect((int) position.x, (int) position.y, (int) size.x, (int) size.y, 
-        //         (int) (size.x * 0.1), (int) (size.y * 0.1));
-        // }
         for (int i = 0; i < pilha.size(); i++) {
-            int y = (int) (position.y - (size.y * (i + 1))) - (SPACEMENT * (i + 1));
+            int y = (int) (position.y - (size.y * (i + 1))) - (((SPACEMENT*3) * (i + 1)) + 25);
             int x = (int) (position.x - (size.x / 2));
+            g2.drawString("Index: " + Integer.toString(i), (int) x + 25, (int) y);
+            g2.drawString("Valor: " + pilha.get(i), (int) x + 25, (int) y + 61);
             g2.fillRoundRect(x, y, (int) size.x, (int) size.y, (int) (size.x * 0.1), (int) (size.y * 0.1));
+
+            if(i == 0){
+                g2.drawImage(arrowImg, x - 120, y - 25, null);
+                g2.drawString("Inicio", x - 110, y + 10);
+            }
+            if(i == pilha.size() - 1){
+                g2.rotate(Math.toRadians(-180), x + ARROW_IMG_WIDTH/2, y + ARROW_IMG_HEIGHT/2);
+                g2.drawImage(arrowImg, x - 120, y + 25, null);
+                g2.rotate(-Math.toRadians(-180), x + ARROW_IMG_WIDTH, y + ARROW_IMG_HEIGHT);
+                g2.drawString("Topo", x + 300, y + 115);
+            }
         }
     }
 
